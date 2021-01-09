7c43d9754c1cc31b0f159e58785f97f495796b45
ejw773 <ejw773@gmail.com>
2021-01-09 09:50:32 -0500

    creates a simple single endpoint

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
+app.get('/heartbeat', (req, res) => {
+    res.json({
+        status: 'eat@home server is running'
+    })
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server is listening on port ${port}`)
+})
