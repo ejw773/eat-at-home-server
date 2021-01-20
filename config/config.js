ae4ec90c4ae0abaa733164f08645eac261e3775f
ejw773 <ejw773@gmail.com>
2021-01-20 13:13:24 -0500

    fixes broken endpoints

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "wilcott",
+    "password": null,
+    "database": "eat-at-home-server",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "wilcott",
+    "password": null,
+    "database": "eat-at-home-server",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "wilcott",
+    "password": null,
+    "database": "eat-at-home-server",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
