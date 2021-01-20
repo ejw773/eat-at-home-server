a27ec7ea22e7e5ec3b5a3c4eda16948990f129f0
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-19 21:58:43 -0500

    updating reviews model and migrations file

--- migrations/20210113023808-create-reviews.js
+++ migrations/20210113023808-create-reviews.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      reviews: {
+      review: {
         type: Sequelize.STRING
       },
       company_id: {
