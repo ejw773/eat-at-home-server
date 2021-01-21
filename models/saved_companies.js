27fa56f36923e56e5d7834eb01223d20dff7a153
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-21 17:00:18 -0500

    setting up model/table associations

--- models/saved_companies.js
+++ models/saved_companies.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Saved_Companies.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        onDelete:'CASCADE'
+      });
     }
   };
   Saved_Companies.init({
