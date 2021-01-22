216f3d53862c88df14477768f5552b2668773f3e
ejw773 <ejw773@gmail.com>
2021-01-22 18:10:43 -0500

    minor config.js changes

--- config/config.js
+++ config/config.js
@@ -39,3 +39,28 @@ module.exports = {
     }
   }
 }
+
+//
+// module.exports = {
+//   "development": {
+//     "username": "wilcott",
+//     "password": null,
+//     "database": "eat-at-home_db",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "test": {
+//     "username": "wilcott",
+//     "password": null,
+//     "database": "eat-at-home_db",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "production": {
+//     "username": "wilcott",
+//     "password": null,
+//     "database": "eat-at-home_db",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   }
+// }
