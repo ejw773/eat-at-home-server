7d58905a1202581b253f1e6e77525c7abd9ce77a
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-24 00:36:08 -0500

    updating models and package.json

--- models/reviews.js
+++ models/reviews.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Reviews.belongsTo(models.User, {
-        foreignKey: 'userId',
+        foreignKey: 'user_id',
         onDelete: 'CASCADE'
       });
     }
